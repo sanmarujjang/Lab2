651a188d310532ced98a200889dd00bce0f053a5
yechan <yechengogo@snu.ac.kr>
2025-10-08 09:47:04 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
 
+    int start_num = 1;
+    if (!vec.empty()) {
+        start_num = vec.back() + 1;
+    }
 
-
-
-
-
-
+    for (int i = start_num; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+    func(0);
+
+    return 0;
 }
+
